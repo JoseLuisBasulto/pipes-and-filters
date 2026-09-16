43420c845ff8b2d00fc38948051dd6398ae49e84
Jose-Basulto <A24216358@alumnos.uady.mx>
2026-09-16 12:58:11 -0500

    avances en model

--- src/model/Customer.java
+++ src/model/Customer.java
@@ -1,4 +1,25 @@
 package model;
 
 public class Customer {
+    private final int idCustomer;
+    private final String name;
+    private final int telephone;
+
+    public Customer(int idCustomer, String name, int telephone) {
+        this.idCustomer = idCustomer;
+        this.name = name;
+        this.telephone = telephone;
+    }
+
+    public int getIdCustomer() {
+        return idCustomer;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getTelephone(){
+        return telephone;
+    }
 }
